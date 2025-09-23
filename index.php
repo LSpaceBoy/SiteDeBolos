22f138f3ce23d4feb0129f93bc3f0dc110a5101f
Lucas Pace <lucas.leandropace@gmail.com>
2025-09-22 22:20:03 -0300

    Aula-22-09-PHP

--- index.php
+++ index.php
@@ -32,17 +32,9 @@
         <p>&nbsp;</p>
         <p>&nbsp;</p>
     </main>
-
-    <footer class="bg-secondary mt-4">
-
-        <div class="container text-center py-2">
-            <p class="mb-0 small text-white">© 2024 Lalá Cake. Todos os direitos reservados.</p>
-        </div>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
+    <?php
+    include_once("./layout/footer.php");
+    include_once("./layout/script-js.php"); ?>
 </body>
 
 </html>
